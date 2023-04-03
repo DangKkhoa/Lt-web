18e76df88a8a764f6183528dd106f07006ca8292
dangkkhoa310 <dangkkhoa10a8@gmail.com>
2023-04-03 23:03:30 +0700

    Update files

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// function myFunc() {
+//     const items = document.querySelectorAll('.side-content');
+//     items.forEach((item) => {
+//         item.addEventListener('click', () => {
+//             console.log('you clicked '+ i);
+//         })
+        
+//     });
+    
+
+    
+
+
+
+    
+
+
+// }
+
+
+let items = document.querySelectorAll('.side-content');
+let prev_id = [];
+items.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        
+    
+    })
+
+    
+
+
+
+
+})
+
+
+
+
+
+
+
